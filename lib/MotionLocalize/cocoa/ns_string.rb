405ad9610b5e1791abd49e1a5c978beea5e1d1d6
Benjamin Michotte <bmichotte@gmail.com>
2015-07-30 21:21:11 +0200

    add warning

--- lib/MotionLocalize/cocoa/ns_string.rb
+++ lib/MotionLocalize/cocoa/ns_string.rb
@@ -3,12 +3,12 @@ class NSString
   def translate(opts={})
     str = NSBundle.mainBundle.localizedStringForKey(self, value: nil, table: nil)
     unless str
+      mp missing_translation: self, force_color: :red
       return self
     end
 
     str = str.dup
     opts.each do |key, value|
-      puts "replace #{key} with #{value}"
       str.gsub!("%{#{key}}", value.respond_to?(:to_s) ? value.to_s : value)
     end
     str
